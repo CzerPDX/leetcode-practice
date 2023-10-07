5f9771b4a4cf1acaa9e31c939c14aea2bb7bf982
czer <czerpdx@gmail.com>
2023-10-07 15:43:10 -0700

    added working solution for reverseInteger

--- reverse-integer/reverseInteger.cpp
+++ reverse-integer/reverseInteger.cpp
@@ -2,11 +2,42 @@
 #include <iostream>
 using namespace std;
 
+/*
+ From Leetcode:
+ Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+
+ Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ 
+ Example 1:
+
+ Input: x = 123
+ Output: 321
+ Example 2:
+
+ Input: x = -123
+ Output: -321
+ Example 3:
+
+ Input: x = 120
+ Output: 21
+  
+
+ Constraints:
+
+ -231 <= x <= 231 - 1
+ */
+
 int main(int argc, char** argv) {
   Solution *aSolution = new Solution();
 
   cout << aSolution->reverse(1234) << endl;
-
+  
+  cout << aSolution->reverse(-123) << endl;
+  
+  cout << aSolution->reverse(-2147483412) << endl;
+  
+  cout << aSolution->reverse(-2147483648) << endl;
+  
   delete aSolution;
 
   return 0;
--- reverse-integer/solution.cpp
+++ reverse-integer/solution.cpp
@@ -8,20 +8,31 @@ using namespace std;
 
 int Solution::reverse(int x) {
   int retVal = 0;
-  int modNum = 10;
-
-  if ((x <= -8463847412) || (x >= 7463847412)) {
-    return 0;
-  }
-
-  while (x > 0) {
-    retVal = retVal + (x % 10);
+    
+  while (x != 0) {
+    // First grab the digit off the end of x
+    int endDigit = x % 10;
+    
+    // Then check for overflow/underflow
+    // It will overflow next time if
+      // The retVal is greater than 214748364
+      // OR retVal is equal to 214748364 AND the endDigit is greater than 7
+    // It will underflow next time if
+      // The retVal is less than -214748364
+      // OR retVal is equal to -214748364 AND the endDigit is greater than 8
+    if (retVal > 214748364 || (retVal == 214748364 && endDigit > 7)) {
+      return 0;
+    }
+    if (retVal < -214748364 || (retVal == -214748364 && endDigit < -8)) {
+      return 0;
+    }
+    
+    // Multiply retVal by 10 to give space for the endDigit, then add the endDigit onto retVal
+    retVal = (retVal * 10) + endDigit;
+    // Remove endDigit from the end of x
     x /= 10;
-    retVal *= 10;
   }
-
-  retVal /= 10;
-
+  
   return retVal;
 }
 
