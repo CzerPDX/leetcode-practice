48a2afe0886130edbfe4502d720a0887cecc7a86
czer <czerpdx@gmail.com>
2023-10-12 17:17:11 -0700

    updated 3sum to two poitners approach and used logic instead of set to avoid duplicates

--- 3sum/solution.cpp
+++ 3sum/solution.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <set>
 #include <algorithm>
+#include <map>
 
 #include "solution.h"
 
@@ -8,28 +9,52 @@ using namespace std;
 
 vector<vector<int>> Solution::threeSum(vector<int>& nums) {
   vector<vector<int>> retVectors;
+  // set<vector<int>> uniqueVectors; // Using a set to collect vectors so there are no duplicates
+  
+  // Sort incoming nums array
+  sort(nums.begin(), nums.end());
 
-  // Sort nums to make it easier to find values
-  sort(nums.begin(), nums.end()); 
+  // Going to try the 2 pointer approach:
 
-  // Slow version first
-  for (vector<int>::iterator first = nums.begin(); first != nums.end(); first++) {
-    for (vector<int>::iterator second = first + 1; second != nums.end(); second++) {
-      int secondSum = *first + *second;
-      bool thirdFound = false;
-      for (vector<int>::iterator third = second + 1; ((third != nums.end()) && (!thirdFound)); third++) {
-        if ((secondSum + *third) == 0) {
-          thirdFound = true;
-          vector<int> vectorToAdd = {*first, *second, *third};
-          retVectors.push_back(vectorToAdd);
+  for (int i = 0; i < nums.size(); i++) {
+    // Skip duplicates of the first number
+    if ((i == 0) || (nums[i] != nums[i - 1])) {
+      int left = i + 1;
+      int right = nums.size() - 1;
+
+      while (left < right) {
+        // Want to look for a combination of left + right that adds up to -nums[i]
+        // Move left to the right if it's too small.
+        // Move right to the left if it's too large.
+        // Save it if it's equal
+
+        int sum = nums[left] + nums[right];
+        if (sum == (nums[i] * -1)) {
+          retVectors.push_back({ nums[i], nums[left], nums[right]});
+          // uniqueVectors.insert({ nums[i], nums[left], nums[right] });
+
+          // Advance left pointer and decrement right pointer till they aren't the same value they currently are
+          while((left < right) && (nums[left] == nums[left + 1])) {
+            left++;
+          }
+          while((left < right) && (nums[right] == nums[right - 1])) {
+            right--;
+          }
+
+          // Then advance them once more
+          left++;
+          right--;
+        } else if (sum < (nums[i] * -1)) {
+          left++;
+        } else {
+          right--;
         }
       }
     }
   }
 
-  // Get rid of duplidates
-  set<vector<int>> uniqueVectors(retVectors.begin(), retVectors.end());
-  retVectors.assign(uniqueVectors.begin(), uniqueVectors.end());
+  // Assign retVectors to the unique vectors start end end
+  // retVectors.assign(uniqueVectors.begin(), uniqueVectors.end());
 
   return retVectors;
 }
